2309a05de1e34732ff2a15b677b69f1e5301a0a0
mjrj97 <martin.jensen.1997@hotmail.com>
2021-11-04 12:19:14 +0100

    Add project files.

---
+++ Getting Real/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Getting_Real
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
