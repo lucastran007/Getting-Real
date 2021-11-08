eafc6689a141fb0dc9b20df5e3a7fdf06bbead92
mjrj97 <mjrj38934@edu.ucl.dk>
2021-11-08 11:14:08 +0100

    Added tenant class and repo.

---
+++ Getting Real/Tenant.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Getting_Real
+{
+    public class Tenant
+    {
+        private string _Name;
+        public string Name
+        {
+            get { return _Name; }
+            set { _Name = value; }
+        }
+
+        private string _Phone;
+        public string Phone
+        {
+            get { return _Phone; }
+            set { _Phone = value; }
+        }
+
+        private string _Email;
+        public string Email
+        {
+            get { return _Email; }
+            set { _Email = value; }
+        }
+
+        private DateTime _MoveInDate;
+        public DateTime MoveInDate
+        {
+            get { return _MoveInDate; }
+            set { _MoveInDate = value; }
+        }
+
+        private DateTime _MoveOutDate;
+        public DateTime MoveOutDate
+        {
+            get { return _MoveOutDate; }
+            set { _MoveOutDate = value; }
+        }
+
+        public Tenant(string Name, string Phone, string Email, DateTime MoveInDate, DateTime MoveOutDate)
+        {
+            this.Name = Name;
+            this.Email = Email;
+            this.Phone = Phone;
+            this.MoveInDate = MoveInDate;
+            this.MoveOutDate = MoveOutDate;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name},{Phone},{Email},{MoveInDate},{MoveOutDate}";
+        }
+    }
+}
---
+++ Getting Real/TenantRepo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Getting_Real
+{
+    public class TenantRepo
+    {
+        private List<Tenant> _Tenants;
+        public List<Tenant> Tenants
+        {
+            get { return _Tenants; }
+            set { _Tenants = value; }
+        }
+
+        private string path = "../../../Tenants.txt";
+
+        public TenantRepo()
+        {
+            Load();
+        }
+
+        public void Save()
+        {
+            using (StreamWriter writer = new(path))
+            {
+                for (int i = 0; i < Tenants.Count; i++)
+                {
+                    writer.WriteLine(Tenants[i]);
+                }
+            }
+        }
+
+        public void Load()
+        {
+            using (StreamReader reader = new(path))
+            {
+                Tenants = new List<Tenant>();
+
+                string line;
+                while ((line = reader.ReadLine()) != null) {
+                    string[] data = line.Split(',');
+                    Tenants.Add(new Tenant(data[0], data[1], data[2], DateTime.Parse(data[3]), DateTime.Parse(data[4])));
+                }
+            }
+        }
+    }
+}
